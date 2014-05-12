bca0aee8e8e8be3047b2c8570bd8a7db8c8c5a9e
Stéphane P. Péricat <Stephane.Pericat@spilgames.com>
2014-05-12 11:47:13 +0200

    load SPIL API in Game Maker!

---
+++ extensions/SpilAPI/GameAPI.js
@@ -0,0 +1,21 @@
+function SpilAPI_injectAPI(callback) {
+	var s = document.createElement('script');
+	s.src = 'http://cdn.gameplayer.io/api/js/game.js';
+	s.id = 'spil-api';
+	document.getElementsByTagName('head')[0].appendChild(s);
+
+	s.onload = function() {
+		callback.call(this);
+	};
+}
+
+function SpilAPI_loadAPI() {
+	SpilAPI_injectAPI(function() {
+		if(window && window.GameAPI) {
+			window.GameAPI.loadAPI(function(instance) {
+				console.log('SPIL Game API v' + instance.version + ' loaded in Game Maker!');
+				return instance;
+			});
+		}
+	});
+}
