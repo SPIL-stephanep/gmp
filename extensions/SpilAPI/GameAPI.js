3a857d4a24d4409d69ba324f526f13276d89cd66
Stéphane P. Péricat <Stephane.Pericat@spilgames.com>
2014-05-23 13:35:11 +0200

    WIP callback stuff

--- extensions/SpilAPI/GameAPI.js
+++ extensions/SpilAPI/GameAPI.js
@@ -14,6 +14,7 @@ function SpilAPI_loadAPI(AppID) {
 		if(window && window.GameAPI) {
 			window.GameAPI.loadAPI(function(instance) {
 				console.log('SPIL Game API v' + instance.version + ' loaded in Game Maker!');
+				execute_callback('pauseGame', "API loaded!");
 				return true;
 			}, {id: AppID});
 		} else {
@@ -45,4 +46,57 @@ function SpilAPI_getLink(linkName) {
 function SpilAPI_listLinks () {
 	var linkList = window.GameAPI.Branding.listLinks();
 	return JSON.stringify(linkList);
+}
+
+function execute_callback(cbName, data) {
+	//console.log(cb, typeof cb);
+	if(gmCallback[cbName]) {
+		gmCallback[cbName](data);
+	}
+}
+
+//======= GM Callback v1.1 ========
+//By JacksonYarr
+//Website: http://JacksonYarr.com/
+//License: Creative Commons Attribution 3.0 Unported License
+//=================================
+
+var gmCallback = new Object();
+
+function callback_script(extname,numargs)
+{
+	if(gmCallback[extname] != undefined)
+	{return true;}
+	
+	var obFunc = window["callback_script"].caller.name;
+
+	if(obFunc == undefined) //IE always makes things difficult.
+	{obFunc = arguments.callee.caller.toString().match(/function ([^\(]+)/)[1];}
+	
+	var args = "";
+	var gmres = "0,0";
+	
+	if(numargs > 0)
+	{
+		while(numargs > 0)
+		{
+			args = "arg"+numargs+","+args;
+			numargs--;
+		}
+		
+		args = args.slice(0,-1);
+		gmres = gmres+",";
+	}
+	
+	gmCallback[extname] = eval("(function("+ args +"){"+ obFunc +"("+ gmres + args +");})");
+	
+	return false;
+}
+
+function callback_define_script(extname,gmscript)
+{
+	if(gmCallback[extname] != undefined)
+	{return true;}
+	
+	return false;
 }
---
+++ scripts/pauseGame.gml
@@ -0,0 +1,6 @@
+if(!callback_script("pauseGame",0)) {
+    exit;
+}
+
+show_message("I got called back!");
+
